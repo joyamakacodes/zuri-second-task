5cec090013e852c2c72a1e09ca81f77ddc03d981
joy onweani <joyamaka1995@gmail.com>
2022-11-05 08:54:06 +0100

    feature: added new feature contact

---
+++ src/Screens/Contact.jsx
@@ -0,0 +1,22 @@
+
+import Input from "../Components/Input"
+import Footer from '../Components/Footer'
+
+const Contact = ()=>{
+    
+    
+    return(
+        <div className="contactBody">
+                <div className="contact">
+        <div className="formContainer">
+            <h2>Contact Me </h2>
+            <p className="contactPara">Hi there, contact me to ask me about anything you have in mind.</p>
+            <Input/>
+        </div>
+    </div>
+    <Footer/>
+        </div>
+
+    )
+}
+export default Contact
